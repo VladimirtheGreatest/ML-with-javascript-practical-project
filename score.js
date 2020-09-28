038500f6031720da8349d58fb4d24ec8ea3337d8
Vladimir <vladkodoktorov@gmail.com>
2020-09-28 16:00:19 +0100

    added analysis for k-nearest neighbour algorhitm

--- score.js
+++ score.js
@@ -1,12 +1,29 @@
 const outputs = [];
+const k = 3;
+const predictionPoint = 300;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
-  console.log(outputs);
-
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+ const bucket =  _.chain(outputs)
+  .map(row => [distance(row[0]), row[3]])
+  .sortBy(row => row[0])
+  .slice(0, k)
+  .countBy(row => row[1])
+  .toPairs()
+  .sortBy(row => row[1])
+  .last()
+  .first()
+  .parseInt()
+  .value()
+
+  console.log('Your point will probably fall int', bucket)
 }
+
+function distance(point){
+  return Math.abs(point - predictionPoint);
+}
+
 
