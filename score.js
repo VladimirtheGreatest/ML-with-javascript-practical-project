1f6da88070155d77cc523b758816ebfedd9af352
Vladimir <vladkodoktorov@gmail.com>
2020-09-28 17:34:43 +0100

    generalizing knn randomizing etst data

--- score.js
+++ score.js
@@ -1,14 +1,17 @@
 const outputs = [];
 const k = 3;
-const predictionPoint = 300;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 }
 
 function runAnalysis() {
- const bucket =  _.chain(outputs)
-  .map(row => [distance(row[0]), row[3]])
+ const bucket =  console.log('Your point will probably fall int', bucket)
+}
+
+function knn(data, point){
+  return _.chain(data)
+  .map(row => [distance(row[0], point), row[3]])
   .sortBy(row => row[0])
   .slice(0, k)
   .countBy(row => row[1])
@@ -17,13 +20,22 @@ function runAnalysis() {
   .last()
   .first()
   .parseInt()
-  .value()
-
-  console.log('Your point will probably fall int', bucket)
+  .value();
 }
 
-function distance(point){
-  return Math.abs(point - predictionPoint);
+function distance(pointA, pointB){
+  return Math.abs(pointA - pointB);
+}
+
+function splitDataset(data, testCount){
+  const shuffled = _.shuffle(data);
+
+
+  //we will use testing set and training set to test accuracy of our algorhitm
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return[testSet, trainingSet]
 }
 
 
