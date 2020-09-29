b45803db70ed8450d434f4481d9223f5a774639b
Vladimir <vladkodoktorov@gmail.com>
2020-09-29 16:50:59 +0100

    refactored distance function to use pythagorean theorem

--- score.js
+++ score.js
@@ -8,9 +8,9 @@ function runAnalysis() {
  const testSetSize = 100;
  const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 
- _.range(1,20).forEach(kElement => {
+ _.range(1,20).forEach(kElement => { //making prediction for more k elements
  const accuracy = _.chain(testSet)
-  .filter(testPoint => knn(trainingSet, testPoint[0], kElement) === testPoint[3])
+  .filter(testPoint => knn(trainingSet, testPoint[0], kElement) === testPoint[3])//comparing values
    .size()
     .divide(testSetSize)
     .value()
@@ -18,6 +18,7 @@ function runAnalysis() {
   });
 }
 
+//k nearest neighbour algorithm
 function knn(data, point, k){
   return _.chain(data)
   .map(row => [distance(row[0], point), row[3]])
@@ -33,7 +34,14 @@ function knn(data, point, k){
 }
 
 function distance(pointA, pointB){
-  return Math.abs(pointA - pointB);
+  //using 3d pythagorean theorem to calculate distance between points x² + y² + z² = s²!
+  // pointA = [300, .5, 16] pointB = [350, .55, 16]
+  return _.chain(pointA)
+  .zip(pointB)  // example _.zip(['a', 'b'], [1, 2], [true, false]); => [['a', 1, true], ['b', 2, false]]
+  //Creates an array of grouped elements, the first of which contains the first elements of the given arrays, the second of which contains the second elements of the given arrays, and so on.
+  .map(([a,b]) => (a - b) ** 2) 
+  .sum()
+  .value() ** 0.5;
 }
 
 function splitDataset(data, testCount){
@@ -41,8 +49,11 @@ function splitDataset(data, testCount){
 
 
   //we will use testing set and training set to test accuracy of our algorhitm
+                          //array/start position / end position
   const testSet = _.slice(shuffled, 0, testCount);
+  console.log('test set',testSet);
   const trainingSet = _.slice(shuffled, testCount);
+  console.log('training set', trainingSet);
 
   return[testSet, trainingSet]
 }
