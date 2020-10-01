1c12d4de613b51b12bed763f38e46dcd8a01bca8
Vladimir <vladkodoktorov@gmail.com>
2020-10-01 15:37:06 +0100

    added feature normalization with minmax formula

--- score.js
+++ score.js
@@ -6,11 +6,11 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 
 function runAnalysis() {
  const testSetSize = 100;
- const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
+ const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize);
 
  _.range(1,20).forEach(kElement => { //making prediction for more k elements
  const accuracy = _.chain(testSet)
-  .filter(testPoint => knn(trainingSet, testPoint[0], kElement) === testPoint[3])//comparing values
+  .filter(testPoint => knn(trainingSet, _.initial(testPoint), kElement) === testPoint[3])//comparing values, filter out those that meet the prediction
    .size()
     .divide(testSetSize)
     .value()
@@ -20,6 +20,8 @@ function runAnalysis() {
 
 //k nearest neighbour algorithm
 function knn(data, point, k){
+ // console.log('data in knn, or training set', data)
+  //point will always have 3 values no labels in it
   return _.chain(data)
   .map(row => {
     //initial == Gets all but the last element of array.
@@ -28,8 +30,8 @@ function knn(data, point, k){
        _.last(row) //bucket which it falls into last element of the array
     ];
   })
-  .sortBy(row => row[0])
-  .slice(0, k)
+  .sortBy(row => row[0]) //sort by drop position
+  .slice(0, k) //take the chunk from the array for k= 3 take 3 rows
   .countBy(row => row[1])
   .toPairs()
   .sortBy(row => row[1])
@@ -41,11 +43,14 @@ function knn(data, point, k){
 
 function distance(pointA, pointB){
   //using 3d pythagorean theorem to calculate distance between points x² + y² + z² = s²!
-  // pointA = [300, .5, 16] pointB = [350, .55, 16]
+  // pointA = [300, .5, 16](prediction point???????) pointB = [350, .55, 16]
+  //console.log('pointA', pointA);
+  //console.log('pointB', pointB);
   return _.chain(pointA)
   .zip(pointB)  // example _.zip(['a', 'b'], [1, 2], [true, false]); => [['a', 1, true], ['b', 2, false]]
   //Creates an array of grouped elements, the first of which contains the first elements of the given arrays, the second of which contains the second elements of the given arrays, and so on.
   .map(([a,b]) => (a - b) ** 2) 
+  // prediction point(training set) - the actual point(testing set)
   .sum()
   .value() ** 0.5;
 }
@@ -64,4 +69,21 @@ function splitDataset(data, testCount){
   return[testSet, trainingSet]
 }
 
+//this function helps us to normalize data
+function minMax(data, featureCount){
+  const clonedData = _.cloneDeep(data);
+
+  for (let index = 0; index < featureCount; index++) {
+    const column = clonedData.map(row => row[index]); //get the 1st values of each array 
+    const min =_.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      //formula for normalization
+      clonedData[j][index] = (clonedData[j][index] - min) / (max -min); //applying normalization for the biggest value 1 for the smallest 0
+      
+    }
+  }
+  return clonedData;
+}
 
